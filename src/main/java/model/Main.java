224bcaf3faa4ea292ecb15cb6da94b68223622c6
leyli <hosseinzadeleyli.job@gmail.com>
2021-03-09 18:36:02 +0330

    third commit for log4j

--- src/main/java/model/Main.java
+++ src/main/java/model/Main.java
@@ -1,10 +1,14 @@
 package model;
 
+import org.apache.log4j.Logger;
+
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
 
+
 public class Main {
+    private static Logger logger = Logger.getLogger("model.Main");
     public static void main(String[] args) {
         Path path1 = Paths.get("files/pardakht.txt");
         Path path2 = Paths.get("files/mojoodi.txt");
@@ -16,7 +20,7 @@ public class Main {
         String payable = fh.isPayable(toHashMapPrdkht, toHashMapMojd);
         String updatedMojoodi = "";
         if(payable!=null){
-            System.out.println("you can pay with this "+payable.substring(payable.indexOf("\t"),payable.length())+
+            logger.info("you can pay with this "+payable.substring(payable.indexOf("\t"),payable.length())+
                     "   deposite number!");
             updatedMojoodi = fh.updateDAccounts(toHashMapPrdkht, toHashMapMojd);
             updatedMojoodi = updatedMojoodi.concat(fh.updateCAccounts(toHashMapPrdkht, toHashMapMojd));
